1f470b9aaa410cefaad6a135324af3a56225d8a9
you22fy <uni22yu@gmail.com>
2025-11-27 15:18:21 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_python_utils',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ]
+)
